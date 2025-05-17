aef4fa2d6a7856474359ac5e9e46ec306cee9c94
PlainOlAir <33238486+PlainOlAir@users.noreply.github.com>
2025-05-17 03:56:59 -0700

    Updated main
    General layout of MacCormack, build external functions/scripts as code is written

--- main.m
+++ main.m
@@ -3,3 +3,26 @@ clear
 addpath('lib\')
 
 setup
+
+dt = 2.35e-11; % Constant time step (s)
+
+time = 0;
+
+for step = 1:1500
+    U = prim2cons(rho(:, :, step), u(:, :, step), v(:, :, step), T(:, :, step), cv);
+    if mod(step, 2) == 1
+        
+    else
+    end
+
+    if mod(step, 2) == 1
+
+    else
+    end
+end
+
+function E = xflux(U, p, T, cp, Pr, dx, dy)
+end
+
+function F = yflux(U, p, T, cp, Pr, dx, dy)
+end
