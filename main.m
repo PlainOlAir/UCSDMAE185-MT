0c5481da5f0fb97216b0880264b76029da2bb4eb
nwpalmtree <nwpalmieri@gmail.com>
2025-05-18 13:01:52 -0700

    Added predictor/corrector structure, following code organization laid out by prof, added animator structure and fixed fluxes slightly.

--- main.m
+++ main.m
@@ -8,31 +8,60 @@ dt = 2.35e-11; % Constant time step (s)
 
 time = 0;
 
+%% --- Main Loop ---
 for step = 1:step_total
-    % primitive var's and U update
-    U(:,:,step) = prim2cons(rho(:, :, step), u(:, :, step), v(:, :, step), T(:, :, step), cv);
-    % I/O and visualization
+    %% I/O, loop updates, delta_t_CFL, visualization
+    a = sqrt(gamma*R*T);
     % compute delta_t CFL
+    %% Alternator
+    if mod(step, 2) == 1
+        FD_method_pred = 'fwd';
+        FD_method_corr = 'bwd';
+    else
+        FD_method_pred = 'bwd_bias';
+        FD_method_corr = 'fwd_bias';
+    end
     %% Predictor
-    if mod(step, 2) == 1
-        FD_method = 'fwd';
-    else
-        FD_method = 'bwd';
-    end
-    [E(:,:,step), F(:,:,step)] = flux(U(:,:,step),dx,dy,FD_method);
+    % update mu, k
+    mu = sutherland(T, mu0, T0, S1);
+    k = (cp/Pr)*mu;
+    % compute derivatives, update E, F
+    [E, F] = flux(U,dx,dy,FD_method_pred);
+    % compute U_bar using U, E, F
     U_bar = U(:,:,step) - dt.*(E(:,:,step) + F(:,:,step));
-    [rho_bar, u_bar, v_bar, T_bar, p_bar, e_bar, Et_bar] = cons2prim(U_bar, R, cv);
-    
-    % Update U using the predicted values
-    U_pred = prim2cons(rho_bar, u_bar, v_bar, T_bar, cv);
-    
-    %% Corrector
-    if mod(step, 2) == 1
+    % compute primitive var's from U_bar
+    [rho_bar, u_bar, v_bar, T_bar, p_bar, e_bar, Et_bar] = cons2prim(U_bar, R, cv); 
+    % enforce BC's on p, u,v, T (update rho, e,...)
 
-    else
-    end
+
+    %% Corrector
+    [E, F] = flux(U_pred,dx,dy,FD_method_corr);
+
+    %% Compute U from primitive vars
+
+end
+%% --- Animator ---
+%% Draw figure
+figure;
+tile = tiledlayout(1,1);
+axesArray = [nexttile(tile,1)];
+h = [pcolor(axesArray(1),xx, yy, squeeze(U(:,:,1)))];
+for ax = axesArray
+    shading(ax, 'interp');
+    axis(ax, 'equal', 'tight');
+    xlabel(ax, 'x');
+    ylabel(ax, 'y');
+    colorbar(ax);
+    % clim(ax,[0 2])
+end
+titles = [title(axesArray(1),'', 'Interpreter', 'latex')];
+for i = 1:10:step_total
+    set(h(1), 'CData', squeeze(U(:,:,i)));
+    titles(1).String = sprintf('$\\ U \\ at \\ t=%.3f s \\ (%d/%d)$',t(1,i), i, step_total);  % Update title
+    drawnow;
 end
 
+%% --- FUNCTIONS ---
 function [E, F] = flux(U, dx, dy, FD_method) 
 % need gradient and bias eq's
     [rho, u, v, T, p, ~, Et] = cons2prim(U, R, cv);
@@ -42,24 +71,24 @@ function [E, F] = flux(U, dx, dy, FD_method)
         tau_yy = 2.*(mu.*ddx_fwd(v,dy,2,true) - (1/3).*gradient.*v);
         qx = -k.*ddx_fwd(T,dx,1,true);
         qy = -k.*ddx_fwd(T,dy,2,true);
-    elseif strcmpi(FD_method,'bwd') == 1 % corrector
+    elseif strcmpi(FD_method,'bwd') == 1 % predictor
         tau_xy = mu(ddx_bwd(u,dy,2,true) + ddx_bwd(v,dx,1,true));
         tau_xx = 2.*(mu.*ddx_bwd(u,dx,1,true) - (1/3).*gradient.*u);
         tau_yy = 2.*(mu.*ddx_bwd(v,dy,2,true) - (1/3).*gradient.*v);
         qx = -k.*ddx_bwd(T,dx,1,true);
         qy = -k.*ddx_bwd(T,dy,2,true);
-    elseif strcmpi(FD_method,'fwd_bias') == 1
-        % tau_xy = mu(ddx_bwd(u,dy,2,true) + ddx_bwd(v,dx,1,true));
-        % tau_xx = 2.*(mu.*ddx_bwd(u,dx,1,true) - (1/3).*gradient.*u);
-        % tau_yy = 2.*(mu.*ddx_bwd(v,dy,2,true) - (1/3).*gradient.*v);
-        % qx = -k.*ddx_bwd(T,dx,1,true);
-        % qy = -k.*ddx_bwd(T,dy,2,true);
-    elseif strcmpi(FD_method,'bwd_bias') == 1
-        % tau_xy = mu(ddx_bwd(u,dy,2,true) + ddx_bwd(v,dx,1,true));
-        % tau_xx = 2.*(mu.*ddx_bwd(u,dx,1,true) - (1/3).*gradient.*u);
-        % tau_yy = 2.*(mu.*ddx_bwd(v,dy,2,true) - (1/3).*gradient.*v);
-        % qx = -k.*ddx_bwd(T,dx,1,true);
-        % qy = -k.*ddx_bwd(T,dy,2,true);
+    elseif strcmpi(FD_method,'fwd_bias') == 1 % corrector
+        tau_xy = mu(ddx_central(u,dy,2,true) + ddx_bwd(v,dx,1,true));
+        tau_xx = 2.*(mu.*ddx_fwd(u,dx,1,true) - (1/3).*gradient.*u);
+        tau_yy = 2.*(mu.*ddx_fwd(v,dy,2,true) - (1/3).*gradient.*v);
+        qx = -k.*ddx_fwd(T,dx,1,true);
+        qy = -k.*ddx_fwd(T,dy,2,true);
+    elseif strcmpi(FD_method,'bwd_bias') == 1 % corrector
+        tau_xy = mu(ddx_fwd(u,dy,2,true) + ddx_central(v,dx,1,true));
+        tau_xx = 2.*(mu.*ddx_bwd(u,dx,1,true) - (1/3).*gradient.*u);
+        tau_yy = 2.*(mu.*ddx_bwd(v,dy,2,true) - (1/3).*gradient.*v);
+        qx = -k.*ddx_bwd(T,dx,1,true);
+        qy = -k.*ddx_bwd(T,dy,2,true);
     else
         error('Incorrect or no FD method given.')
     end
