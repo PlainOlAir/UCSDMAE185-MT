c1ba4afc0030446b37d3df2a2dd8d21cc752cb52
nwpalmtree <nwpalmieri@gmail.com>
2025-05-20 14:48:28 -0700

    added numerical schlieren function

---
+++ num_schlieren.m
@@ -0,0 +1,14 @@
+figure;
+
+beta = 0.8;
+kappa = 10;
+gradp = sqrt(ddx_central(output_vars{5}(:,:,step_total),dx,1).^2 + ddx_central(output_vars{5}(:,:,step_total),dy,2).^2);
+S = beta*exp(-kappa*(gradp./(max(gradp))));
+pcolor(xx,yy,S)
+shading('interp')
+colormap(gray)
+colorbar
+clim([0, 1])
+xlabel('$x$', 'Interpreter','latex')
+ylabel('$y$', 'Interpreter','latex')
+title('Numerical Schlieren','Interpreter','latex')
