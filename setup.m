8f8628922da43947ac9ffdaca5f43963a1ae57ae
nwpalmtree <nwpalmieri@gmail.com>
2025-05-19 17:00:47 -0700

    update: changed U from a 3D to 4D matrix, comments in setup.m explain how

--- setup.m
+++ setup.m
@@ -10,6 +10,9 @@ mu0 = 1.735e-5;  % Dynamic viscosity (N-s/m^2)
 S1 = 110.4;     % Sutherland's temperature (K)
 Pr = 0.71;      % Prandtl number 
 
+a = sqrt(gamma * R * T0);
+uinf = a * M;
+
 %% Grid setup
 M = 4;      % Mach Number
 nx = 75;    % x Number of points
@@ -20,13 +23,13 @@ H = 8e-6;   % Height of computational domain (m)
 dx = L / nx;
 dy = H / ny;
 
-[x, y] = ndgrid(linspace(0, L, nx), linspace(0, H, ny));
-[u,v,p,rho,T,e] = deal(zeros(nx, ny));
-
-a = sqrt(gamma * R * T0);
-uinf = a * M;
-
 step_total = 1500;
+
+[x, y] = ndgrid(linspace(0, L, nx), linspace(0, H, ny));
+
+%% Preallocation
+[u,v,p,rho,T,e,U_bar] = deal(zeros(nx, ny));
+U = zeros(4,nx,ny,step_total);
 
 %% Initial Conditions
 u(:, :) = uinf;
@@ -53,7 +56,11 @@ v(:, end) = 0;
 p(:, end) = p0;
 T(:, end) = T0;
 
-%% Preallocation
-[U, Ubar, Upred] = deal(zeros(4,nx,ny,step_total));
+% in general:
+% U = 4 x Nx x Ny x step array of conservative variables:
+%   1 - density
+%   2 - x-mass flux
+%   3 - y-mass flux
+%   4 - total energy
 % set U(:,:,:,step 1) to BC's
 U(:,:,:,1) = prim2cons(rho,u,v,T,cv);
