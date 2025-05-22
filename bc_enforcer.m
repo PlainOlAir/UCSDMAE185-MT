1951219c23d863e09a2a03eaa18aa978c08a8e81
PlainOlAir <33238486+PlainOlAir@users.noreply.github.com>
2025-05-22 00:57:15 -0700

    Boundary condition fixes
    Leading edge boundary condition was getting overwritten. Changed boundary condition limits to exclude the leading edge & moved leading edge boundary condition lower down as redundancy.
    
    Temporary (or not):
    Extrapolated far field to diagnose rho*v residual being significantly larger than other conservative variables

--- bc_enforcer.m
+++ bc_enforcer.m
@@ -6,28 +6,32 @@ function [rho, u, v, T, p, e, Et] = bc_enforcer(u, v, T, p, cv, R, uinf, pinf, T
     T(end,:) = 2*(T(end-1,:)) - T(end-2,:);
 
     % Bottom wall
-    u(:, 1) = 0;
-    v(:, 1) = 0;
-    T(:, 1) = Tinf;
-    p(:, 1) = 2*(p(:,2)) - p(:,3);
+    u(2:end, 1) = 0;
+    v(2:end, 1) = 0;
+    T(2:end, 1) = Tinf;
+    p(2:end, 1) = 2*(p(2:end,2)) - p(2:end,3);
+
+    % Inlet
+    u(1, 2:end) = uinf;
+    v(1, 2:end) = 0;
+    p(1, 2:end) = pinf;
+    T(1, 2:end) = Tinf;
+    
+    % Far field
+    % u(:, end) = uinf;
+    % v(:, end) = 0;
+    % p(:, end) = pinf;
+    % T(:, end) = Tinf;
+    u(:, end) = 2*u(:, end-1) - u(:, end-2);
+    v(:, end) = 2*v(:, end-1) - v(:, end-2);
+    p(:, end) = 2*p(:, end-1) - p(:, end-2);
+    T(:, end) = 2*T(:, end-1) - T(:, end-2);
 
     % Leading edge
     u(1,1) = 0;
     v(1,1) = 0;
     p(1,1) = pinf;
     T(1,1) = Tinf;
-    
-    % Inlet
-    u(1, :) = uinf;
-    v(1, :) = 0;
-    p(1, :) = pinf;
-    T(1, :) = Tinf;
-    
-    % Far field
-    u(:, end) = uinf;
-    v(:, end) = 0;
-    p(:, end) = pinf;
-    T(:, end) = Tinf;
 
     rho = p./(R.*T);
     e = cv.*T;
